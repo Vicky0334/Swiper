fee3fd1c188fe06cfac9425b7b404c815fb717fd
Vicky Dhote <dhoteji034@gmail.com>
2024-07-09 09:15:47 +0530

    anima swaip

---
+++ script.js
@@ -0,0 +1,11 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView:1,
+    spaceBetween: 5,
+    freeMode: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable:true,
+      
+    },
+  });
+  
